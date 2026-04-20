752f13ff61a1c5f7e145b3666699873c4de0d2da
eulloa23 <eulloa23@ilg.cat>
2026-04-20 22:09:49 +0200

    exercici1

---
+++ replicador/config/database.py
@@ -0,0 +1,14 @@
+import psycopg2
+
+def connection_db():
+    conn = psycopg2.connect(
+        database="postgres",
+        password="pass_postgres",
+        user="user_postgres",
+        host="localhost",
+        port="5432"
+    )
+    print("Conexion establecida")
+    return conn
+
+connection_db()
